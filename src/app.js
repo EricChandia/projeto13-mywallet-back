4a1345402c3bfcb104843cd78a92f4b1d4353daf
EricChandia <eric.chandia.ti@gmail.com>
2022-07-04 00:25:09 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+import cors from 'cors';
+dotenv.config();
+
+import { signUp, signIn, getCredentials } from './controller/authController.js';
+import { newIn } from './controller/newInController.js';
+import { getRecords } from './controller/recordsController.js';
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect(() => {
+  db = mongoClient.db("db_my_wallet");
+});
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post("/sign-up", signUp);
+app.post("/sign-in", signIn);
+app.get("/meus-dados", getCredentials);
+app.post("/newIn", newIn);
+app.get("/getRecords", getRecords);
+
+
+
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000.');
+});
